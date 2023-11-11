71c4c9537f6eb0e2968b47c41a156fa7bdebb61f
zzZMortganZzz <mikhaylov-kg@mail.ru>
2023-11-11 11:29:19 +1000

    Create project

---
+++ ConsoleApplication_Test/ConsoleApplication_Test/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_Test
+{
+    class Program
+    {
+        static List<string> _repo = new List<string>();
+        static int Len = 3;
+
+
+        static void Main(string[] args)
+        {
+            string tmp = string.Empty;
+            Console.WriteLine("Тестовое итоговое задание.");
+            do
+            {
+                Console.Write("Введите слово: ");
+                tmp = Console.ReadLine();
+            } while (FillList(tmp));
+
+            Console.Write("Слова длинной {0} символа:", Len);
+
+            string rep = string.Join(", ", _repo);
+            string rez = string.Join(", ", _repo.Where(item=>item.Length==Len));
+            Console.WriteLine(string.Format("[{0}] => [{1}]", rep, rez));
+            Console.ReadLine();
+
+
+            
+        }
+
+        static bool FillList(string value)
+        { 
+            if(value.Length!=0)
+            {
+                _repo.Add(value);
+                return true;
+            }
+            return false; 
+        }
+
+        //static List<string> GetItem()
+        //{
+        //    List<string> _rezult = new List<string>();
+        //    foreach (string item in _repo)
+        //    {
+        //        if (item.Length == Len)
+        //        {
+        //            _rezult.Add(item);
+        //        }
+        //    }
+        //    return _rezult;
+        //}
+
+    }
+}
---
+++ ConsoleApplication_Test/ConsoleApplication_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ConsoleApplication_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApplication_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("86f4b0e8-9f12-4ce7-91a2-e188a2cb0d2e")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
